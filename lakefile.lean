51be54e5f7dcb33d648558f0078c79b8f9f3887c
Peiyang-Song <p_song@ucsb.edu>
2023-12-28 10:04:56 -0800

    Duper + Lean Copilot

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,7 @@ open Lake DSL
 
 package «duperDemo» {
   -- add any package configuration options here
+  moreLinkArgs := #["-L./.lake/packages/LeanCopilot/.lake/build/lib", "-lctranslate2"] -- for Lean Copilot
 }
 
 require «mathlib» from git "https://github.com/leanprover-community/mathlib4" @ "v4.4.0-rc1"
@@ -13,3 +14,5 @@ require «Duper» from git "https://github.com/leanprover-community/duper.git" @
 lean_lib «DuperDemo» {
   -- add library configuration options here
 }
+
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.0.1"
